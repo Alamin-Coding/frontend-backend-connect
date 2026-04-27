4649fa93772e1ce507d32b096a0001ada72d22f0
Alamin-Coding <md.alamin.coding@gmail.com>
2026-04-27 19:57:20 +0600

    create customer

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,80 +1,112 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
+const Customer = require("../models/customer.model");
 const sendMail = require("../utils/sendMail");
 
 const registration = async (req, res) => {
-	const { username, email, password } = req.body;
-	const newUser = new User({
-		username,
-		email,
-		password,
-	});
+  const { username, email, password } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
 
-	await newUser.save();
+  await newUser.save();
 
-	sendMail(email, username, newUser._id);
+  // sendMail(email, username, newUser._id);
 
-	res.status(201).json({
-		message: "User registered successfully",
-		user: newUser,
-	});
+  res.status(201).json({
+    message: "User registered successfully",
+    user: newUser,
+  });
 };
 
 const login = async (req, res) => {
-	const { email, password } = req.body;
-	const isUserExist = await User.findOne({ email: email });
-	console.log(isUserExist);
+  const { email, password } = req.body;
+  const isUserExist = await User.findOne({ email: email });
+  console.log(isUserExist);
 
-	// user not found
-	if (!isUserExist) {
-		return res.status(404).send({
-			message: "invalid credentials",
-		});
-	}
+  // user not found
+  if (!isUserExist) {
+    return res.status(404).send({
+      message: "invalid credentials",
+    });
+  }
 
-	const passwordMatch = await isUserExist.comparePassword(password);
-	console.log(passwordMatch);
+  const passwordMatch = await isUserExist.comparePassword(password);
+  console.log(passwordMatch);
 
-	if (!passwordMatch) {
-		return res.status(404).send({
-			message: "invalid credentials",
-		});
-	}
+  if (!passwordMatch) {
+    return res.status(404).send({
+      message: "invalid credentials",
+    });
+  }
 
-	res.status(201).json({
-		message: "User logged in successfully",
-		email: isUserExist.email,
-	});
+  let token = jwt.sign(
+    { id: isUserExist._id, role: isUserExist.role, name: isUserExist.username },
+    process.env.ACCESS_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  res.status(201).json({
+    message: "User logged in successfully",
+    token: token,
+  });
 };
 
 // Verification email
 const verify = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const user = await User.findById(id);
+  const user = await User.findById(id);
 
-	if (!user) {
-		return res.status(404).send({
-			message: "User not found",
-		});
-	}
+  if (!user) {
+    return res.status(404).send({
+      message: "User not found",
+    });
+  }
 
-	user.isVerified = true;
-	await user.save();
+  user.isVerified = true;
+  await user.save();
 
-	return res.status(200).send({
-		message: "Email verified successfully",
-		userID: user._id,
-	});
+  return res.status(200).send({
+    message: "Email verified successfully",
+    userID: user._id,
+  });
 };
 
 const getAllUsers = async (req, res) => {
-	const users = await User.find();
-	return res.status(200).send(users);
+  const users = await User.find();
+  return res.status(200).send(users);
+};
+
+// create customer
+const createCustomer = async (req, res) => {
+  const { name, email, password, gender, address, phone } = req.body;
+
+  const newCustomer = await new Customer({
+    name,
+    email,
+    password,
+    gender,
+    address,
+    phone,
+  });
+
+  await newCustomer.save();
+
+  res.status(201).json({
+    message: "Customer created successfully",
+    customer: newCustomer,
+  });
 };
 
 module.exports = {
-	registration,
-	login,
-	verify,
-	getAllUsers,
+  registration,
+  login,
+  verify,
+  getAllUsers,
+  createCustomer,
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 const {
-	registration,
-	verify,
-	getAllUsers,
-	login
+  registration,
+  verify,
+  getAllUsers,
+  login,
+  createCustomer,
 } = require("../controllers/auth.controllers");
 
 const express = require("express");
+const roleValidationMiddleware = require("../middleware/roleValidation.middleware");
 
 const authRouter = express.Router();
 // /api/v1/auth/register
@@ -14,5 +16,11 @@ authRouter.post("/login", login);
 // verify email route
 authRouter.get("/users", getAllUsers);
 authRouter.get("/verify/:id", verify);
+authRouter.post("/create-customer", roleValidationMiddleware, createCustomer);
+// customer editing 
+
+// customer deleting
+
+
 
 module.exports = authRouter;
